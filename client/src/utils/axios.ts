332b97601716572734181f931f315bb66ac4e9a7
Marst313 <dlegoinyoman1@gmail.com>
2024-11-27 20:43:14 +0700

    update status

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const customFetch = axios.create({
-  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
-  // baseURL: 'http://localhost:3000/api/v1',
+  // baseURL: import.meta.env.VITE_APP_API_BASE_URL,
+  baseURL: 'http://localhost:3000/api/v1',
 })
 
 export const customFetchAudio = axios.create({
